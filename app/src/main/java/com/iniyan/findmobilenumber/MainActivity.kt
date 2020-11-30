19410767656d4bfb7e68b28e0c0bba24cf034ddf
iniyan <iniyan455@gmail.com>
2020-11-30 15:18:14 +0530

    Code Test

--- app/src/main/java/com/iniyan/findmobilenumber/MainActivity.kt
+++ app/src/main/java/com/iniyan/findmobilenumber/MainActivity.kt
@@ -1,64 +1,35 @@
 package com.iniyan.findmobilenumber
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
-import android.content.Intent
-import android.content.IntentSender
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
+import android.widget.TextView
 import android.widget.Toast
-import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.credentials.Credential
-import com.google.android.gms.auth.api.credentials.HintRequest
-import com.google.android.gms.common.api.GoogleApiClient
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var googleApiClient: GoogleApiClient
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tryGetCurrentUserPhoneNumber(this)
-//        googleApiClient = GoogleApiClient.Builder(this).addApi(Auth.CREDENTIALS_API).build()
-//        if (phoneNumber.isEmpty()) {
-//            val hintRequest = HintRequest.Builder().setPhoneNumberIdentifierSupported(true).build()
-//            val intent = Auth.CredentialsApi.getHintPickerIntent(googleApiClient, hintRequest)
-//            try {
-//                startIntentSenderForResult(intent.intentSender, REQUEST_PHONE_NUMBER, null, 0, 0, 0);
-//            } catch (e: IntentSender.SendIntentException) {
-//                Toast.makeText(this, "failed to show phone picker", Toast.LENGTH_SHORT).show()
-//            }
-//        } else
-            onGotPhoneNumberToSendTo()
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_PHONE_NUMBER) {
-            if (resultCode == Activity.RESULT_OK) {
-                val cred: Credential? = data?.getParcelableExtra(Credential.EXTRA_KEY)
-                phoneNumber = cred?.id ?: ""
-                if (phoneNumber.isEmpty())
-                    Toast.makeText(this, "failed to get phone number", Toast.LENGTH_SHORT).show()
-                else
-                    onGotPhoneNumberToSendTo()
-            }
-        }
+        onGotPhoneNumberToSendTo()
+        findViewById<TextView>(R.id.textView).text = "$msg got number:$phoneNumber"
     }
 
     private fun onGotPhoneNumberToSendTo() {
-        Toast.makeText(this, "got number:$phoneNumber", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$msg got number:$phoneNumber", Toast.LENGTH_SHORT).show()
     }
 
 
     companion object {
-        private const val REQUEST_PHONE_NUMBER = 1
         private var phoneNumber = ""
-
+        private var msg : String = ""
         @SuppressLint("MissingPermission", "HardwareIds", "ServiceCast")
         private fun tryGetCurrentUserPhoneNumber(context: Context): String {
             if (phoneNumber.isNotEmpty())
@@ -70,17 +41,18 @@ class MainActivity : AppCompatActivity() {
                         val number: String? = it.number
                         if (!number.isNullOrBlank()) {
                             phoneNumber = number
+                            msg = "subscriptionManager"
                             return number
                         }
                     }
-                } catch (ignored: Exception) {
-                }
+                } catch (ignored: Exception) {}
             }
             try {
                 val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
                 val number = telephonyManager.line1Number ?: ""
                 if (!number.isBlank()) {
                     phoneNumber = number
+                    msg = "telephonyManager"
                     return number
                 }
             } catch (e: Exception) {
